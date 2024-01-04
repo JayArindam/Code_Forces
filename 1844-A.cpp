eadb4c2071d28e5dfc5f30307fb8ccb79ac8d88b
JayArindam <jayarindammaity08@gmail.com>
2024-01-04 23:49:44 +0530

    1844-A.cpp

---
+++ 1844-A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+#define ll long long
+ 
+using namespace std;
+ 
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+}
+ 
+int main() {
+    ll T;
+    cin >> T;
+ 
+    while (T--) {
+        solve();
+    }
+ 
+    return 0;
+}
